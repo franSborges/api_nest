faf89d3bd9801b86855f652ea84b1be22b437f5c
franSborges <borgesfran988@gmail.com>
2022-08-20 20:18:15 -0300

    :refactor: files

---
+++ src/modules/users/user.controller.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { User } from './user.entity';
+import { UserService } from './user.service';
+
+@Controller('users')
+export class UserController {
+
+  // private userService = new UserService();
+  constructor(private userService: UserService) {}
+
+  @Get(':username')
+  public searchByUsername(@Param('username') username: string): User {
+       const userFound = this.userService.searchByUsername(username);
+       return userFound;
+  }
+
+  @Post()
+  public create(@Body() users: User): User {
+       const userCreated = this.userService.create(users);
+       return userCreated;
+  }
+}
